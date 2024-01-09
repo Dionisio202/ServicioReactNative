41202ee38dc5d0c031a39f2a54652c5e2467ee00
Edison <solisedison@outlook.com>
2024-01-09 12:35:02 -0500

    db

--- Dockerfile
+++ Dockerfile
@@ -5,7 +5,7 @@ WORKDIR /var/www/html/
 
 # Copia el código de tu aplicación al directorio del servidor web en el contenedor
 COPY . /var/www/html/
-RUN chmod -R 755 /var/www/html/
 
 RUN docker-php-ext-install pdo_mysql
 
+EXPOSE 80
--- conexion.php
+++ conexion.php
@@ -3,12 +3,13 @@ header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
 class Conexion{
     public function Conectar(){
-        define("host","localhost");
-        define("dbname","quinto");
-        define("username","root");
-        define("password","");
+        define("host","reactnative-save-money.a.aivencloud.com");
+        define("dbname","defaultdb");
+        define("username","avnadmin");
+        define("password","AVNS_LiaFmepI5cpT0nJA0r4");
        try{
-    $con = new PDO("mysql:host=".host.";dbname=".dbname,username,password);
+    $con = new PDO("mysql:host=".host.";port=13599;dbname=".dbname, username, password);
+
     return $con;
        }catch(PDOException $e){
         die("error :".$e->getMessage());
